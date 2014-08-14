37a635049302e28f41e7eb4ec99ddfe9dccf2432
wislon <wislon@gmail.com>
2014-08-15 07:52:48 +1000

    Add '(dis)connecting' image, tho on fast devices you barely see it. Auto-generated resources dictionary for same

--- src/BluetoothToggleWidget/Resources/Resource.Designer.cs
+++ src/BluetoothToggleWidget/Resources/Resource.Designer.cs
@@ -48,13 +48,16 @@ namespace BluetoothToggleWidget
 			public const int bluetooth_connected = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int bluetooth_off = 2130837505;
+			public const int bluetooth_connecting = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int bluetooth_on = 2130837506;
+			public const int bluetooth_off = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int icon = 2130837507;
+			public const int bluetooth_on = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int icon = 2130837508;
 			
 			static Drawable()
 			{
