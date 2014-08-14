eb4428496ce63b659fceedd4d64d7cf041bf720f
wislon <wislon@gmail.com>
2014-08-14 18:56:14 +1000

    Added image references and rebuilt resource file

--- src/BluetoothToggleWidget/Resources/Resource.Designer.cs
+++ src/BluetoothToggleWidget/Resources/Resource.Designer.cs
@@ -45,13 +45,16 @@ namespace BluetoothToggleWidget
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int bluetooth_off = 2130837504;
+			public const int bluetooth_connected = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int bluetooth_on = 2130837505;
+			public const int bluetooth_off = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int icon = 2130837506;
+			public const int bluetooth_on = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int icon = 2130837507;
 			
 			static Drawable()
 			{
